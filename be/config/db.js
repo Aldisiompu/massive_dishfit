02f38838fc62b73f024551801522746965d8d9ae
ditayuliantii <ydita97@gmail.com>
2023-12-18 20:21:28 +0700

    modified node modules

--- be/config/db.js
+++ be/config/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("test", "root", "", {
+const db = new Sequelize("dishfit", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
