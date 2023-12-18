6526a7f704a2cc22aa6b968524b8cc021e8b83e7
ditayuliantii <ydita97@gmail.com>
2023-12-18 20:53:24 +0700

    add data vegetarian diabetik

---
+++ fe/src/data/dataVegetarian.jsx
@@ -0,0 +1,39 @@
+export const dataVegatarian = [
+    {
+      imgCard: "img/diet/vegetarian/1.jpg",
+      title: "Bircher Muesli",
+      jenis: "Makanan Utama",
+      url: "/dvegetarian1",
+    },
+    {
+      imgCard: "img/diet/vegetarian/2.jpg",
+      title: "Oatmeal Instan",
+      jenis: "Makanan Utama",
+      url: "/dvegetarian2",
+    },
+    {
+      imgCard: "img/diet/vegetarian/3.jpg",
+      title: "Ubi Panggang",
+      jenis: "Hidangan Penutup",
+      url: "/dvegetarian3",
+    },
+    {
+      imgCard: "img/diet/vegetarian/4.jpg",
+      title: "Sapo Tahu",
+      jenis: "Makanan Utama",
+      url: "/dvegetarian4",
+    },
+    {
+      imgCard: "img/diet/vegetarian/5.jpg",
+      title: "Tumis Jamur",
+      jenis: "Makanan Utama",
+      url: "/dvegetarian5",
+    },
+    {
+      imgCard: "img/diet/vegetarian/6.jpg",
+      title: "Milkshake kurma + susu almond",
+      jenis: "Hidangan Penutup",
+      url: "/dvegetarian6",
+    },
+  ];
+  
